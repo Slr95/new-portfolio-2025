3922d26f9eebd11f84155b1e27cebf28f3d22a4b
Renzo Soler <renzo.soler@esolutions.com.ar>
2024-12-09 14:17:03 -0300

    tailwind config vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
   mode: "jit",
   theme: {
     extend: {
